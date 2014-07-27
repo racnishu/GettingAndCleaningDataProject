9868644455bb7e08e7140ac6c5b03f74f6f780b4
unknown <rachit.kulshrestha@BLRVS5440-Ravindra.fkipl.flipkart.com>
2014-07-27 15:33:19 +0530

    Reading all data files- First Edit

---
+++ run_analysis.r
@@ -0,0 +1,13 @@
+setwd("C:/Users/rachit.kulshrestha/Personal/Courses/Getting and Cleaning Data/")
+
+#Reading all data file in the Data folder to work on the cleaning of these files.
+training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
+training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
+training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+
+testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
+testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
+testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+
+activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+
